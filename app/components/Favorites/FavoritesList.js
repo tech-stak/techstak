dd4bb51ce8798ec900854829366684a455ad8466
Chase Ellsworth <chaseme3@gmail.com>
2015-03-14 00:00:14 -0600

    add FavoritesList component

--- app/components/Favorites/FavoritesList.js
+++ app/components/Favorites/FavoritesList.js
@@ -1,1 +1,16 @@
 var React = require('react');
+
+var FavoritesList = React.createClass({
+	render: function(){
+		var favorites = this.props.favorites.map(function(favorite, index){
+			return <li className="list-group-item" key={index}>{favorite}</li>
+		});
+		return (
+			<ul className="list-group">
+				{favorites}
+			</ul>
+		)
+	}
+});
+
+module.exports = FavoritesList;
