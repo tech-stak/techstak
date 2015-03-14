b5d38ea45197bfecd823360eafe45ecaaa708474
Chase Ellsworth <chaseme3@gmail.com>
2015-03-13 23:56:06 -0600

    add Favorites component

--- app/components/Favorites/Favorites.js
+++ app/components/Favorites/Favorites.js
@@ -1,1 +1,39 @@
 var React = require('react');
+var favoritesStore = require('../../stores/favoritesStore');
+var AddFavorite = require('../AddFavorite');
+var FavoritesList = require('./FavoritesList');
+var favoritesActions = require('../../actions/favoritesActions');
+
+var Favorites = React.createClass({
+	getInitialState: function(){
+		return {
+			favorites: favoritesStore.getState().favorites
+		};
+	},
+	componentWillReceiveProps: function(obj){
+		favoritesActions.changeUser(obj.username);
+	},
+	componentDidMount: function(){
+		favoritesActions.changeUser(this.props.username);
+		favoritesStore.addChangeListener(this._onChange);	
+	},
+	componentWillUnmount: function(){
+		favoritesStore.removeChangeListener(this._onChange);
+	},
+	render: function(){
+		return (
+			<div>
+				<h3> Favorites for {this.props.username} </h3>
+				<AddFavorite username={this.props.username} />
+				<FavoritesList favorites={this.state.favorites} />
+			</div>
+		);
+	},
+	_onChange: function(){
+		this.setState({
+			favorites: favoritesStore.getState().favorites;
+		})
+	}
+});
+
+module.exports = Favorites;
