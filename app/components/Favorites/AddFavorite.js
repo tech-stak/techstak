471ab9e0c175227d51c3e6a3c941b3e17a52a68b
Chase Ellsworth <chaseme3@gmail.com>
2015-03-13 23:37:49 -0600

    add AddFavorite component

--- app/components/Favorites/AddFavorite.js
+++ app/components/Favorites/AddFavorite.js
@@ -1,1 +1,23 @@
 var React = require('react');
+var favoritesActions = require();
+var favoritesStore = require();
+
+var AddFavorite = React.createClass({
+	handleSubmit: function(){
+		////////FINISH	
+	},
+	render: function(){
+		return (
+			<div className="input-group cushion">
+				<input type="text" ref="note" className="form-control" placeholder="Add to Favorites" />
+				<span className="input-group-btn">
+					<button type="btn btn-default" type="button" onClick={this.handleSubmit}>Add</button>
+				</span>
+			</div> 
+		)
+	}
+});
+
+module.exports = AddFavorite;
+
+////WHATS A GENERAL RULE FOR DECIDING WHAT SHOULD BE CAPITALIZED??
