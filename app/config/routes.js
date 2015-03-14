86feb72767fcacd1cd02b4fd43e601b59a74824e
Chase Ellsworth <chaseme3@gmail.com>
2015-03-13 23:29:07 -0600

    add routes

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,1 +1,16 @@
+var React = require('react');
+var Main = require('../components/Main');
+var Home = require('../components/Home');
+//DO WE NEED TO REQUIRE ANYTHING ELSE HERE??
+var Profile = require('../components/Profile');
+var Router = require('react-router');
+var DefaultRoute = Router.DefaultRoute;
+var Route = Router.Route;
+//DO WE NEED TO REQUIRE ANYTHING ELSE HERE??
 
+module.exports = (
+	<Route name="app" path="/" handler={Main}>
+		<Route name="profile" path="profile/:username" handler={Profile} />
+		<DefaultRoute handler={Home} />
+	</Route>
+);
