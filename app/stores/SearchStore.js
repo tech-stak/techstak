16d83626ca2c118c1ea8a5c3f0708e4419823482
Alex Hawkins <alexhawkins.me@gmail.com>
2015-03-13 23:29:35 -0600

    implement store for job searches

---
+++ app/stores/SearchStore.js
@@ -0,0 +1,44 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var appConstants = require('../constants/appConstants');
+var objectAssign = require('react/lib/Object.assign');
+var EventEmitter = require('events').EventEmitter;
+
+var CHANGE_EVENT = 'change';
+
+var _state = {
+  jobs: []
+};
+
+var setJobs = function(data) {
+  _state.jobs = data.jobs; //returns an array of jobs
+};
+
+var searchStore = objectAssign({}, EventEmitter.prototype, {
+  addChangeListener: function(cb) {
+    this.on(CHANGE_EVENT, cb);
+  },
+  removeChangeListener: function(cb) {
+    this.removeListener(CHANGE_EVENT, cb);
+  },
+  getState: function() {
+    return _state;
+  };
+});
+
+AppDispatcher.register(function(payload) {
+
+  var action = payload.action;
+
+  switch (action.actionType) {
+
+    case appConstants.ANGEL_GET_JOBS:
+      setJobs(action.data);
+      searchStore.emit(CHANGE_EVENT);
+      break;
+
+    default:
+      return true;
+  }
+});
+
+module.exports = githubStore;
