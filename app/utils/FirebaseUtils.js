33ad4b05537501eba88c422e595d25dc178d33a5
Chase Ellsworth <chaseme3@gmail.com>
2015-03-13 22:52:04 -0600

    add Actions and begin components

--- app/utils/FirebaseUtils.js
+++ app/utils/FirebaseUtils.js
@@ -1,12 +1,19 @@
 var Firebase = require('firebase');
 var appConstants = require('../constants/appConstants');
 
+//What will firebase look like after adding one favorite?
+//What will firebase look like after adding one favorites?
+//How do I delete from Firebase
 var firebaseUtils = {
 	homeInstance: function(){
 		return new Firebase(appConstants.FIREBASE_HOST);
 	},
 	addFavorite: function(favObj){
-		this.homeInstance().child(favObj.user).push(favObj.note);
+		this.homeInstance().child(favObj.user).push(favObj);
+	},
+	///////////FINISH////////
+	deleteFavorite: function(favObj){
+		this.homeInstance().child(favObj.user).child('favorites').child(favObj.favToDelete).remove();
 	},
 	toArray: function(obj){
 		var array = [];
