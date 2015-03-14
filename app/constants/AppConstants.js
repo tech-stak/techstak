fff97ecf782bf104e2f8d0a31f99620a7df31f40
Alex Hawkins <alexhawkins.me@gmail.com>
2015-03-13 23:12:30 -0600

    implement angelList functionality and job requests by technology

--- app/constants/AppConstants.js
+++ app/constants/AppConstants.js
@@ -4,7 +4,8 @@ var appConstants = {
 	CHANGE_USER: 'CHANGE_USER',
 	GITHUB_USER_BIO: 'GITHUB_USER_BIO',
 	GITHUB_CHANGE_USER: 'GITHUB_CHANGE_USER',
-	FIREBASE_HOST: 'https://techstak.firebaseio.com'
+	FIREBASE_HOST: 'https://techstak.firebaseio.com',
+  ANGEL_GET_JOBS: 'ANGEL_GET_JOBS'
 };
 
 module.exports = appConstants;
---
+++ app/data/skills.js
@@ -0,0 +1,22 @@
+var skillTags = {
+  'react' : 139914,
+  'react.js' : 139914,
+  'javascript': 14781,
+  'mongodb' : 16999,
+  'node.js' : 17000,
+  'redis' : 21691,
+  'java' : 14780,
+  'android' : 16680,
+  'html' : 15592,
+  'css' : 15593,
+  'coffeescript' : 21133,
+  'ruby' : 17184,
+  'mysql' : 16309,
+  'linux' : 31586,
+  'php' : 14776,
+  'python' : 14775,
+  'angular.js' : 81410,
+  'angularjs' : 87663
+};
+
+module.exports = skillTags;
